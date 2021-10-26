2f8364ff18286fc4559dba49ce85e8c66b893a1d
A G <babla13@gmail.com>
2021-10-26 16:00:47 +1100

    Changed the code
     - added new endpoints
     - changed port
     - added log messages

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,42 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/status")
+def health_check():
+    health_status = {
+    	"result": "OK - healthy"
+    }
+    response = app.response_class(
+        response=json.dumps(health_status),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    data = {
+        "status":"success",
+        "code":0,
+        "data" : {
+            "UserCount": 140,
+            "UserCountActive": 23,
+        }
+    }
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
